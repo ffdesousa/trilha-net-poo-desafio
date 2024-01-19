da3411ce3f6abc260c4948bd7db983c07008b21d
Fernando Fernandes <contato@fernando-fernandes.com>
2024-01-19 17:44:58 -0300

    Implementando desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,18 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        protected string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria )
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +24,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+         public void InformacoesSmartphone()
+        {
+            Console.WriteLine($"Numero {Numero}, Modelo {Modelo}, IMEI {Imei}, Quatidade de Memoria {Memoria}mb");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"+55(67)9999-9999", modelo:"Nokia 5150", imei:"123456789", memoria:2);
+nokia.InformacoesSmartphone();
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("---------------------");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:"+55(67)9999-9998", modelo:"Iphone 11 Pro", imei:"1111111111111", memoria:64);
+iphone.InformacoesSmartphone();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DuoLingo");
